cd7e9aca247e0801279461535255908d95f326e9
aki <aki@xps>
2011-12-17 23:20:11 +0900

    add bench and lineSplitter

---
+++ src/com/akjava/gwt/lib/client/LineSplitter.java
@@ -0,0 +1,46 @@
+package com.akjava.gwt.lib.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
+
+
+
+public class LineSplitter implements RepeatingCommand{
+private List<String> lines=new ArrayList<String>();
+private int consumeTime;
+private String text;
+private int cindex;
+private SplitterListener listener;
+public LineSplitter(String text,int consumeTime,SplitterListener listener){
+	this.text=text;
+	this.consumeTime=consumeTime;
+	this.listener=listener;
+}
+	@Override
+	public boolean execute() {
+		int splitted=0;
+		long s=System.currentTimeMillis();
+		while(System.currentTimeMillis()<s+consumeTime){
+		int find=text.indexOf("\n", cindex);
+		if(find!=-1){
+			lines.add(text.substring(cindex,find));
+			cindex=find+1;
+		}else{
+			lines.add(text.substring(cindex));
+			listener.onSuccess(lines);
+			return false;
+		}
+		splitted++;
+		}
+		//LogUtils.log("parsed:"+index);
+		return true;
+	}
+
+	public static interface SplitterListener {
+		public void onSuccess(List<String> lines);
+	}
+	
+
+}
