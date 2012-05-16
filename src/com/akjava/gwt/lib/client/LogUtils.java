6fabc78400f32373188008ee827f22e876365d92
aki <aki@xps>
2012-05-16 22:54:38 +0900

    fix ie console problem

--- src/com/akjava/gwt/lib/client/LogUtils.java
+++ src/com/akjava/gwt/lib/client/LogUtils.java
@@ -4,9 +4,19 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class LogUtils {
 	public static final native void log(JavaScriptObject object)/*-{
-	console.log(object);
+		if (navigator.appName == 'Microsoft Internet Explorer'){
+			return;
+		}
+	if(console){
+		console.log(object);
+	}
 	}-*/;
 	public static final native void log(String object)/*-{
-	console.log(object);
+		if (navigator.appName == 'Microsoft Internet Explorer'){
+			return;
+		}
+	if(console){
+		console.log(object);
+	}
 	}-*/;
 }
