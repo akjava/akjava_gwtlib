8af473f4ded32c25df0d84dd4b9be18968d9eea8
aki <aki@xps>
2012-11-30 19:11:05 +0900

    add canvas method

--- src/com/akjava/gwt/lib/client/CanvasUtils.java
+++ src/com/akjava/gwt/lib/client/CanvasUtils.java
@@ -1,6 +1,8 @@
 package com.akjava.gwt.lib.client;
 
 import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.user.client.ui.Image;
 
 public class CanvasUtils {
 public static Canvas createCanvas(int w,int h){
@@ -12,5 +14,21 @@ public static Canvas createCanvas(int w,int h){
 		canvas.setHeight(h+"px");
 	}
 	return canvas;
+}
+/*
+ * if you need use ImageElementLoader
+ */
+public static ImageElement toImageElement(Canvas canvas){
+	Image img=new Image(canvas.toDataUrl());
+	ImageElement imageElement=ImageElement.as(img.getElement());
+	return imageElement;
+}
+
+public static String createColorImageDataUrl(int r,int g,int b,double opacity,int w,int h){
+	Canvas canvas=CanvasUtils.createCanvas(w, h);
+	canvas.getContext2d().setFillStyle("rgba("+r+","+g+","+b+","+opacity+")");
+	canvas.getContext2d().fillRect(0, 0, w, h);
+	String image1=canvas.toDataUrl();
+	return image1;
 }
 }
