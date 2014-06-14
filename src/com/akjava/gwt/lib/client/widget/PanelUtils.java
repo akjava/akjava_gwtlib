5b00b19c630fdf3e0016db41c5a3b4ff11733111
aki <aki@xps>
2014-06-14 23:32:37 +0900

    update panelutils

--- src/com/akjava/gwt/lib/client/widget/PanelUtils.java
+++ src/com/akjava/gwt/lib/client/widget/PanelUtils.java
@@ -1,13 +1,14 @@
 package com.akjava.gwt.lib.client.widget;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class PanelUtils {
 
-	public VerticalPanel createScrolledTabInVerticalPanel(TabLayoutPanel tab,String label){
+	public static VerticalPanel createScrolledTabInVerticalPanel(TabLayoutPanel tab,String label){
 		ScrollPanel scroll=new ScrollPanel();
 		scroll.setSize("98%", "98%");
 		tab.add(scroll,label);
@@ -16,7 +17,25 @@ public class PanelUtils {
 		return panel;
 	}
 	
-	public HorizontalPanel createScrolledTabInHorizontalPanel(TabLayoutPanel tab,String label){
+	public static VerticalPanel createScrolledVerticalPanel(Panel parent){
+		ScrollPanel scroll=new ScrollPanel();
+		
+		parent.add(scroll);
+		VerticalPanel panel=new VerticalPanel();
+		scroll.add(panel);
+		return panel;
+	}
+	
+	public static VerticalPanel createScrolledVerticalPanel98(Panel parent){
+		ScrollPanel scroll=new ScrollPanel();
+		scroll.setSize("98%", "98%");
+		parent.add(scroll);
+		VerticalPanel panel=new VerticalPanel();
+		scroll.add(panel);
+		return panel;
+	}
+	
+	public static HorizontalPanel createScrolledTabInHorizontalPanel(TabLayoutPanel tab,String label){
 		ScrollPanel scroll=new ScrollPanel();
 		scroll.setSize("98%", "98%");
 		tab.add(scroll,label);
