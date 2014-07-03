45d3f8513a277721632fd446c0f1b4e8d44e9c0d
aki <aki@xps>
2014-07-03 22:09:22 +0900

    update

--- src/com/akjava/lib/common/utils/TemplateUtils.java
+++ src/com/akjava/lib/common/utils/TemplateUtils.java
@@ -1,9 +1,21 @@
 package com.akjava.lib.common.utils;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import com.akjava.gwt.lib.client.LogUtils;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
 
 public class TemplateUtils {
+	public final static String TEMPLATE_VALUE_END_STRING="${";
+	public final  static String TEMPLATE_VALUE_START_STRING="}";
+
 	/**
 	 * very simple way just replace ${key} to value;
 	 * @param template
@@ -75,5 +87,71 @@ public class TemplateUtils {
 		return createText(template,map);
 	}
 	
+	public static Set<String> findTemplateKeys(String text){
+		Set<String> keys=new HashSet<String>();
+		if(Strings.isNullOrEmpty(text)){
+			return keys;
+		}
+		
+		int start=0;
+		while(start!=-1){
+			start=text.indexOf(TEMPLATE_VALUE_END_STRING, start);
+			if(start!=-1){
+				int end=text.indexOf(TEMPLATE_VALUE_START_STRING,start+TEMPLATE_VALUE_END_STRING.length());
+				if(end==-1){
+					break;
+				}else{
+					String key=text.substring(start+TEMPLATE_VALUE_END_STRING.length(),end);
+					keys.add(key);
+					LogUtils.log("add:"+key);
+				}
+				start=end+TEMPLATE_VALUE_END_STRING.length();
+			}
+		}
+		return keys;
+	}
 	
+	public static TemplateKey labelToTemplateKey(String label){
+		checkNotNull(label);
+		List<String> vs=Splitter.on("+").splitToList(label);
+		
+		if(vs.size()>1){
+			return new TemplateKey(vs.get(1),vs.get(0));
+		}else{
+			return new TemplateKey(label);
+		}
+	}
+	public static class TemplateKey{
+		private String key;
+		private String option;
+		public TemplateKey(String key) {
+			this(key,null);
+		}
+		public TemplateKey(String key, String option) {
+			super();
+			this.key = key;
+			this.option = option;
+		}
+		public String getKey() {
+			return key;
+		}
+		public void setKey(String key) {
+			this.key = key;
+		}
+		public String getOption() {
+			return option;
+		}
+		public void setOption(String option) {
+			this.option = option;
+		}
+		
+		public String toString(){
+			String header="";
+			if(!Strings.isNullOrEmpty(option)){
+				header=option+"+";
+			}
+			return TEMPLATE_VALUE_START_STRING+header+key+TEMPLATE_VALUE_END_STRING;
+		}
+		
+	}
 }
