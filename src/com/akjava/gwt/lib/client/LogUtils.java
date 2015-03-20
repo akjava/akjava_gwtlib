20b0066dfd74ae67811cd96ad75a6d568205ba4e
Aki Miyazaki <akimisaki3@gmail.com>
2015-03-20 17:04:07 +0900

    support error,warn on Log

--- src/com/akjava/gwt/lib/client/LogUtils.java
+++ src/com/akjava/gwt/lib/client/LogUtils.java
@@ -2,11 +2,12 @@ package com.akjava.gwt.lib.client;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+
 public class LogUtils {
 	public static final native void log(JavaScriptObject object)/*-{
-		if (navigator.appName == 'Microsoft Internet Explorer'){
-			return;
-		}
+if(typeof console === "undefined"){
+    console = { log: function() { },error:function(){},warn:function(){} };
+  }
 	if(console){
 		console.log(object);
 	}
@@ -22,21 +23,81 @@ public class LogUtils {
 			log("null");
 		}
 	}
-	public static final  void log(double value){
+	public static final  void log(Number value){
 		log(""+value);
 	}
-	public static final  void log(long value){
-		log(""+value);
-	}
-	public static final  void log(int value){
-		log(""+value);
-	}
+	
 	public static final native void log(String object)/*-{
-		if (navigator.appName == 'Microsoft Internet Explorer'){
-			return;
-		}
+if(typeof console === "undefined"){
+    console = { log: function() { },error:function(){},warn:function(){} };
+  }
 	if(console){
 		console.log(object);
 	}
 	}-*/;
+	
+	public static final native void error(JavaScriptObject object)/*-{
+if(typeof console === "undefined"){
+    console = { log: function() { },error:function(){},warn:function(){} };
+  }
+if(console){
+	console.error(object);
+}
+}-*/;
+public static final  void error(Object value){
+	if(value!=null){
+	if(value instanceof JavaScriptObject){
+		error((JavaScriptObject)value);
+	}else{
+	error(value.toString());
+	}
+	}else{
+		error("null");
+	}
+}
+public static final  void error(Number value){
+	error(""+value);
+}
+
+public static final native void error(String object)/*-{
+if(typeof console === "undefined"){
+    console = { log: function() { },error:function(){},warn:function(){} };
+  }
+if(console){
+	console.error(object);
+}
+}-*/;
+
+public static final native void warn(JavaScriptObject object)/*-{
+if(typeof console === "undefined"){
+    console = { log: function() { },error:function(){},warn:function(){} };
+  }
+if(console){
+console.warn(object);
+}
+}-*/;
+public static final  void warn(Object value){
+if(value!=null){
+if(value instanceof JavaScriptObject){
+	warn((JavaScriptObject)value);
+}else{
+warn(value.toString());
+}
+}else{
+	warn("null");
+}
+}
+public static final  void warn(Number value){
+warn(""+value);
+}
+
+public static final native void warn(String object)/*-{
+if(typeof console === "undefined"){
+    console = { log: function() { },error:function(){},warn:function(){} };
+  }
+if(console){
+console.warn(object);
+}
+}-*/;
+
 }
